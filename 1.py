3103f667b48f0a9c7864917edd33805287afc0b9
olofenglund96 <olofenglund96@gmail.com>
2017-12-02 14:23:20 +0100

    Test

---
+++ 1.py
@@ -0,0 +1,14 @@
+sum = 0;
+input = "";
+with open("1") as f:
+    for c in f:
+        input += c;
+
+    print(input)
+    for i in range(0, len(input)-1):
+        if(input[i] == input[i+1]):
+            sum += int(input[i])
+
+    sum += 6
+
+print(sum)
---
+++ 2.py
@@ -0,0 +1,16 @@
+sum = 0;
+with open("2") as f:
+    for line in f:
+        line = line.replace('\t', " ")
+        numbers = line.split(" ")
+        maxnum = 0;
+        minnum = 10000;
+        for n in numbers:
+            n = int(n)
+            if(n > maxnum):
+                maxnum = n
+            if(n < minnum):
+                minnum = n
+        print(str(maxnum) + ", " + str(minnum))
+        sum += maxnum-minnum
+print(sum)
